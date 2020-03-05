1833daa3de804d8ab03eb76d30e9556b72ed5738
liumeng <liumeng0403@163.com>
2020-03-05 22:37:46 +0800

    [dart]  main 入口 , 注释,  变量 const final 命名规则

--- First/bin/main.dart
+++ First/bin/main.dart
@@ -1,5 +1,25 @@
-import 'package:First/First.dart' as First;
+// xx
+/// yyy   三个 / 注释 可以方法提示
+void main(){
+  print("你好 dart");
 
-void main(List<String> arguments) {
-  print("first !");
+  var str = "你好 2";
+  print(str);
+
+  String str3 = "你好 3";
+  print(str3);
+
+  int myNum = 12345;
+  print(myNum);
+
+//  myNum =  "";  //  error  /  dart 是强类型语言
+
+  const PI = 3.14;  //  const  编译常量
+  //  PI = 2;   // 常量是不可以修改的
+
+  final time = DateTime.now();  //  final 运行时常量
+
+//  time = DateTime.now();  //  error ,  final 变量 仅可赋值一次
+
+
 }
