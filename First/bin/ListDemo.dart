5f66f9893364daa85854b74604f1fa32892dc052
liumeng <liumeng0403@163.com>
2020-03-13 23:43:44 +0800

    [dart]  List 基本操作

---
+++ First/bin/ListDemo.dart
@@ -0,0 +1,48 @@
+void main() {
+  //
+  List myList = ["香蕉", "苹果", "西瓜"];
+
+  print(myList[1]);
+  print(myList.length);
+  print(myList.isEmpty);
+  print(myList.isNotEmpty);
+  print(myList.reversed.toList());
+  print(myList);
+
+  myList.add("桃子");
+  print(myList);
+
+  myList.addAll(["葡萄1", "葡萄2"]);
+  print(myList);
+
+  print(myList.indexOf("苹果"));
+
+  myList.remove("西瓜");
+  print(myList);
+
+  myList.removeAt(1);
+  print(myList);
+
+  myList.fillRange(1, 3, "x"); //  [ )   //  修改指定位置的值
+  print(myList);
+
+  myList.insert(1, "桃子");
+  print(myList);
+
+  myList.insertAll(0, ["栗子1", "栗子2"]);   //  指定位置插入
+  print(myList);
+
+  var str = myList.join(",");
+  print(str);
+
+  var list2 = str.split(",");
+  print(list2);
+
+  //
+  var list = List();
+  list.add(111);
+  list.add(222);
+  print(list);
+
+  //
+}
