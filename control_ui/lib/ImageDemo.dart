f7c7d4f3613834742e7804ce8f939c04882e905f
liumeng <liumeng0403@163.com>
2020-03-17 21:10:40 +0800

    [3.5] Icon

--- control_ui/lib/ImageDemo.dart
+++ control_ui/lib/ImageDemo.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ImageDemo extends StatelessWidget{
+class ImageDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,19 +14,39 @@ class ImageDemo extends StatelessWidget{
             image: AssetImage("images/avatar.png"),
             width: 100,
           ),
-          Image.asset("images/avatar.png",
-          width: 100,
+          Image.asset(
+            "images/avatar.png",
+            width: 100,
           ),
           // 从网络加载图片
           Image(
-            image: NetworkImage("https://avatars2.githubusercontent.com/u/20411648?s=460&v=4"),
+            image: NetworkImage(
+                "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4"),
             width: 100,
           ),
-          Image.network("https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
-          width: 100,)
+          Image.network(
+            "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
+            width: 100,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.accessible,
+                color: Colors.green,
+              ),
+              Icon(
+                Icons.error,
+                color: Colors.red,
+              ),
+              Icon(
+                Icons.fingerprint,
+                color: Colors.blue,
+              ),
+            ],
+          )
         ],
       ),
     );
   }
-
 }
