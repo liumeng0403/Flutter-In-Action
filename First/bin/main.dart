7f0db00c214f471f5fe1b1993118661bd362808a
liumeng <liumeng0403@163.com>
2020-03-22 19:38:55 +0800

    [dart] 自定义库 第三方库  系统库

--- First/bin/main.dart
+++ First/bin/main.dart
@@ -1,6 +1,12 @@
+import 'dart:math';
+
+import 'dart:io';
+import 'dart:convert';
+
+
 // xx
 /// yyy   三个 / 注释 可以方法提示
-void main01(){
+void main() async {
   print("你好 dart");
 
   var str = "你好 2";
@@ -20,6 +26,18 @@ void main01(){
   final time = DateTime.now();  //  final 运行时常量
 
 //  time = DateTime.now();  //  error ,  final 变量 仅可赋值一次
+
+
+print(min(12,13));
+
+
+var httpClient = new HttpClient();
+var uri = new Uri.http("news-at.zhihu.com", "/api/3/stories/latest");
+
+var request = await httpClient.getUrl(uri);
+var response = await request.close();
+
+print(await response.transform(utf8.decoder).join());
 
 
 }
