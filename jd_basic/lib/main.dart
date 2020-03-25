01b43f74670b9afef865715edc8d82f5d5a798e5
liumeng <liumeng0403@163.com>
2020-03-26 00:23:06 +0800

    [JD] 05 Flutter Container组件、Text组件详解

--- jd_basic/lib/main.dart
+++ jd_basic/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      theme:ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
       home: Scaffold(
@@ -26,13 +26,37 @@ class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        "你好 flutter 111!",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 40,
-//          color: Colors.yellow,
-          color: Color.fromRGBO(244, 121, 121, 0.5),
+      child: Container(
+        height: 300.0,
+        width: 300.0,
+        decoration: BoxDecoration(
+          color: Colors.yellow, //  定义 container 的背景色
+          border: Border.all(
+            //  定义 container 边框颜色
+            color: Colors.blue,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(18)),
+        ),
+        padding: EdgeInsets.all(10),  //  内边距
+        margin: EdgeInsets.all(10),  //  外边距
+        transform: Matrix4.rotationZ(.3),  //   旋转
+        child: Text(
+          "我是一个文本."*10,
+          textAlign: TextAlign.left,
+          overflow: TextOverflow.fade,
+          textScaleFactor: 2,
+          maxLines: 1,
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.red,
+            fontWeight: FontWeight.w800,
+            fontStyle: FontStyle.italic,
+            decoration: TextDecoration.lineThrough,
+            decorationColor: Colors.white,
+            decorationStyle: TextDecorationStyle.dashed,
+            letterSpacing: 5,
+          ),
         ),
       ),
     );
