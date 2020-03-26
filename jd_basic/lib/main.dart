5fd35ff82e4d08ec27c173a6ad533c91d9bdd3fc
liumeng <liumeng0403@163.com>
2020-03-27 00:27:48 +0800

    [JD] 06 Flutter 图片组件Image 、本地图片、远程图片、图片剪切

--- jd_basic/lib/main.dart
+++ jd_basic/lib/main.dart
@@ -27,37 +27,8 @@ class HomeContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        height: 300.0,
-        width: 300.0,
-        decoration: BoxDecoration(
-          color: Colors.yellow, //  定义 container 的背景色
-          border: Border.all(
-            //  定义 container 边框颜色
-            color: Colors.blue,
-            width: 2,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(18)),
-        ),
-        padding: EdgeInsets.all(10),  //  内边距
-        margin: EdgeInsets.all(10),  //  外边距
-        transform: Matrix4.rotationZ(.3),  //   旋转
-        child: Text(
-          "我是一个文本."*10,
-          textAlign: TextAlign.left,
-          overflow: TextOverflow.fade,
-          textScaleFactor: 2,
-          maxLines: 1,
-          style: TextStyle(
-            fontSize: 16,
-            color: Colors.red,
-            fontWeight: FontWeight.w800,
-            fontStyle: FontStyle.italic,
-            decoration: TextDecoration.lineThrough,
-            decorationColor: Colors.white,
-            decorationStyle: TextDecorationStyle.dashed,
-            letterSpacing: 5,
-          ),
-        ),
+        child: Image.asset("images/a.jpeg"),
+
       ),
     );
   }
