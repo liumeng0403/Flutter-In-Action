2840bd9268b02bf01e0439bdedbc709285ddcc4a
liumeng <liumeng0403@163.com>
2020-03-28 23:47:24 +0800

    [JD] 07Flutter ListView基础列表组件、水平列表组件、图标组件

--- jd_basic/lib/main.dart
+++ jd_basic/lib/main.dart
@@ -25,10 +25,48 @@ class MyApp extends StatelessWidget {
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Image.asset("images/a.jpeg"),
-
+    return Container(
+      height: 180,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Container(
+            width: 160,
+            height: 120,
+            color: Colors.red,
+          ),
+          Container(
+            width: 160,
+            height: 120,
+            color: Colors.blue,
+            child: ListView(
+              children: <Widget>[
+                Image.network("https://www.itying.com/images/flutter/1.png"),
+                Text("标题 xxxx"),
+              ],
+            ),
+          ),
+          Container(
+            width: 160,
+            height: 120,
+            color: Colors.yellow,
+          ),
+          Container(
+            width: 160,
+            height: 120,
+            color: Colors.green,
+          ),
+          Container(
+            width: 160,
+            height: 120,
+            color: Colors.black,
+          ),
+          Container(
+            width: 160,
+            height: 120,
+            color: Colors.orange,
+          ),
+        ],
       ),
     );
   }
