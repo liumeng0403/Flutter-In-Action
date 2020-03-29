b9bb62f27ca534d4eb8eceec8ed815228548fe67
liumeng <liumeng0403@163.com>
2020-03-30 00:07:57 +0800

    [JD] 09 flutter GridView组件 以及动态GridView

--- jd_basic/lib/main.dart
+++ jd_basic/lib/main.dart
@@ -24,20 +24,43 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeContent extends StatelessWidget {
-  
-  Widget _getDataList(BuildContext contex,int index){
-    return ListTile(
-      leading: Image.network(listData[index]["imageUrl"]),
-      title: Text(listData[index]["title"]),
-      subtitle: Text(listData[index]["author"]),
+  Widget _getDataList(context, index) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Color.fromRGBO(233, 233, 233, 0.9),
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          Image.network(listData[index]["imageUrl"]),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            listData[index]["title"],
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
       itemCount: listData.length,
       itemBuilder: this._getDataList,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
     );
   }
 }
