864639ec41dead811f1999d1042dffdbaf159fa1
liumeng <liumeng0403@163.com>
2020-03-29 20:44:03 +0800

    [5.2] 尺寸限制类容器 UnconstrainedBox

--- control_ui/lib/containers/ConstrainedSizedDemo.dart
+++ control_ui/lib/containers/ConstrainedSizedDemo.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
-class ConstrainedSizedDemo extends StatelessWidget{
+class ConstrainedSizedDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("尺寸限制类容器 demo"),
+        actions: <Widget>[
+          UnconstrainedBox(
+            child: SizedBox(
+              width: 20,
+              height: 20,
+              child: CircularProgressIndicator(
+                strokeWidth: 3,
+                valueColor: AlwaysStoppedAnimation(Colors.white70),
+              ),
+            ),
+          ),
+        ],
       ),
       body: ListView(
         children: <Widget>[
           ConstrainedBox(
             constraints: BoxConstraints(
-              minWidth: double.infinity,   //宽度尽可能大
+              minWidth: double.infinity, //宽度尽可能大
               minHeight: 50, //最小高度为50像素
             ),
             child: Container(
@@ -19,17 +31,18 @@ class ConstrainedSizedDemo extends StatelessWidget{
               color: Colors.red,
             ),
           ),
-          SizedBox(
-            width: 80,
-            height: 80,
-            child: Container(
-              height: 5,
-              color: Colors.blue,
+          UnconstrainedBox(
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: Container(
+                height: 5,
+                color: Colors.blue,
+              ),
             ),
           ),
         ],
       ),
     );
   }
-
 }
